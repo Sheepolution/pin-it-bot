96cda50a469c34c12779af34014500b66d54b51c
Sheepolution <danielhaazen@hotmail.com>
2021-07-23 21:08:48 +0200

    Add that prefix cache is removed when leaving a guild

--- code/src/Managers/BotManager.ts
+++ code/src/Managers/BotManager.ts
@@ -95,8 +95,8 @@ export default class BotManager {
         LogService.Log(LogType.GuildLeft, guild);
     }
 
-    public static async ClearPrefixCache(messageInfo: IMessageInfo) {
-        var prefixKey = this.prefixKey + messageInfo.message.guild.id;
+    public static async ClearPrefixCache(guildDiscordId: string) {
+        var prefixKey = this.prefixKey + guildDiscordId;
         await Redis.del(prefixKey);
     }
 }
